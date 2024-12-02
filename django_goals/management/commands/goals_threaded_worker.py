9f25d612294b05f0502a0e4912a3eabf31af5fd4
Jan Rydzewski <flegmer@gmail.com>
2024-12-02 13:37:23 +0100

    Restart threads on error

--- django_goals/management/commands/goals_threaded_worker.py
+++ django_goals/management/commands/goals_threaded_worker.py
@@ -1,9 +1,13 @@
+import logging
 import signal
 import threading
 
 from django.core.management.base import BaseCommand
 
 from django_goals.models import worker
+
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -25,14 +29,22 @@ class Command(BaseCommand):
         stop_event = threading.Event()
         signal.signal(signal.SIGINT, lambda signum, frame: stop_event.set())
         signal.signal(signal.SIGTERM, lambda signum, frame: stop_event.set())
-        threads = [WorkerThread(
+        threaded_worker(
+            thread_count=options['threads'],
             stop_event=stop_event,
             once=options['once'],
-        ) for _ in range(options['threads'])]
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
+        )
+
+
+def threaded_worker(thread_count=1, stop_event=None, **kwargs):
+    threads = [WorkerThread(
+        stop_event=stop_event,
+        **kwargs,
+    ) for _ in range(thread_count)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
 
 
 class WorkerThread(threading.Thread):
@@ -42,7 +54,16 @@ class WorkerThread(threading.Thread):
         self.once = once
 
     def run(self):
-        worker(
-            stop_event=self.stop_event,
-            once=self.once,
-        )
+        while (
+            self.stop_event is None or
+            not self.stop_event.is_set()
+        ):
+            try:
+                worker(
+                    stop_event=self.stop_event,
+                    once=self.once,
+                )
+            except Exception as e:
+                logger.exception(e)
+            if self.once:
+                break
--- example_app/merge_sort_tests.py
+++ example_app/merge_sort_tests.py
@@ -2,20 +2,27 @@ import random
 
 import pytest
 
+from django_goals.management.commands.goals_threaded_worker import (
+    threaded_worker,
+)
 from django_goals.models import schedule, worker
 from django_goals.utils import is_goal_completed
 
 from .merge_sort import MergeSort, ensure_sorted
 
 
-@pytest.mark.django_db
-def test_merge_sort():
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.parametrize('worker_function', [
+    worker,
+    lambda **kwargs: threaded_worker(thread_count=4, **kwargs),
+])
+def test_merge_sort(worker_function):
     numbers = [random.randint(0, 100) for _ in range(10)]
     merge_sort = MergeSort.objects.create(
         numbers=numbers,
         goal=schedule(ensure_sorted),
     )
-    worker(once=True)
+    worker_function(once=True)
     merge_sort.refresh_from_db()
     assert is_goal_completed(merge_sort.goal)
     assert merge_sort.sorted_numbers == sorted(numbers)
